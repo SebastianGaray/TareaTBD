df001d62afa8783de56542ca8e89ae2bff331bba
Sebastian <sebastian.garay.p@usach.cl>
2018-04-12 18:09:05 -0300

    Falta arreglar base de datos

---
+++ src/Agregar.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="center">
+   <section class="form">
+            <div class="field">
+              <label class="label">Name</label>
+              <div class="control">
+                <input v-model="form.firstName" class="input" type="text" placeholder="Text input">
+                <input v-model="form.lastName" class="input" type="text" placeholder="Text input2">
+              </div>
+            </div>
+          </section>
+            <button v-on:click="agregar(form)">Add 1</button>
+
+          <div class="column">
+                <section class="section" id="results">
+                  <div class="box">
+                    <ul>
+                      <!-- loop through all the `form` properties and show their values -->
+                      <li v-for="(item, k) in form">
+                        <strong>{{ k }}:</strong> {{ item }}
+                      </li>
+                    </ul>
+                  </div>
+                </section>
+              </div>
+
+
+
+
+  </div>
+</template><script>
+
+
+   export default {
+     data(){
+       return{
+         form : {
+             //   actorId: 1,
+               firstName: '',
+               lastName: '',
+            //    lastUpdate: 1139988873000,
+
+
+             }
+       }
+     },
+     methods:{
+     agregar: function(form){
+        this.$http.post('http://localhost:3000/actors/', [form]);fistNa
+
+     }
+
+
+     },
+
+
+     }
+           </script>
+
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header>
+      <div class="branding">
+        <img class ="logo-small" src="./img/fidget-spinner.svg" />
+        <span>Nagevación superior</span>
+      </div>
+      <nav>
+        <a href="#/">Home</a>
+        <a href="#/actors">Lista</a>
+        <a href="#/agregar">Agregar</a>
+
+      </nav>
+    </header>
+
+    <main class="container">
+      <router-view></router-view>
+    </main>
+    <footer>
+      Footer
+    </footer>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
